e3ffa15b0c59ac784c95b112a7c3e12933aa13f7
Hristo Stefanov <25063600+hristo-stefanov@users.noreply.github.com>
2025-02-23 13:14:09 +0000

    Add tests for MonadicContext

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
     testImplementation(kotlin("test"))
     testImplementation("org.assertj:assertj-core:3.19.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.1")
 }
 
 tasks.test {
---
+++ src/test/kotlin/MonadicContextTest.kt
@@ -0,0 +1,73 @@
+import MonadicContext.expOption
+import MonadicContext.fmaOption
+import MonadicContext.fmaOptionArrow
+import MonadicContext.powFlow
+import MonadicContext.powOption
+import MonadicContext.right
+import MonadicContext.rightValue
+import MonadicContext.some
+import MonadicContext.someValue
+import arrow.core.Some
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.lang.Math.E
+
+class MonadicContextTest {
+    @Test
+    fun testRight() {
+        assertThat(right.isRight()).isTrue()
+    }
+
+    @Test
+    fun testSome() {
+        assertThat(some.isSome()).isTrue()
+    }
+
+    @Test
+    fun testSomeValue() {
+        assertThat(someValue).isEqualTo("some")
+    }
+
+    @Test
+    fun testRightVale() {
+        assertThat(rightValue).isEqualTo("right")
+    }
+
+    @Test
+    fun testExpOption() {
+        val result = expOption(Some(1.0))
+
+        assertThat(result).isEqualTo(Some(E))
+    }
+
+    @Test
+    fun testPowOption() {
+        val result = powOption(Some(2.0), Some(3.0))
+
+        assertThat(result).isEqualTo(Some(8.0))
+    }
+
+    @Test
+    fun testFmaOption() {
+        val result = fmaOption(Some(2.0), Some(3.0), Some(1.0))
+
+        assertThat(result).isEqualTo(Some(7.0))
+    }
+
+    @Test
+    fun testFmaOptionArrow() {
+        val result = fmaOptionArrow(Some(2.0), Some(3.0), Some(1.0))
+
+        assertThat(result).isEqualTo(Some(7.0))
+    }
+
+    @Test
+    fun testPowFlow() = runTest {
+        val result = powFlow(flowOf(2.0), flowOf(3.0))
+
+        assertThat(result.first()).isEqualTo(8.0)
+    }
+}
