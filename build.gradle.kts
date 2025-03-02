b851ebf6b4e532e8dacf0f10e92b1b28fc9db541
Hristo Stefanov <25063600+hristo-stefanov@users.noreply.github.com>
2025-03-02 11:46:06 +0000

    Upgrade to Arrow 2.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,8 @@ repositories {
 }
 
 dependencies {
-    implementation("io.arrow-kt:arrow-core:1.2.4")
+    implementation("io.arrow-kt:arrow-core:2.0.1")
+    implementation("io.arrow-kt:arrow-functions:2.0.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
     testImplementation(kotlin("test"))
     testImplementation("org.assertj:assertj-core:3.19.0")
